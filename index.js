b68c4f6692843459aea60c7ceeb66d3f88a3fb9d
obikym <111091812+obikym@users.noreply.github.com>
2022-11-05 15:14:00 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,41 @@
-const TelegramApi = require('node-telegram-bot-api')
+const TelegramApi = require("node-telegram-bot-api");
 
-const token = '5559401316:AAEF4BdyWEShX7m-IyMq9CPlnx4BuG1C8z0'
+const token = "5559401316:AAEF4BdyWEShX7m-IyMq9CPlnx4BuG1C8z0";
+const bot = new TelegramApi(token, { polling: true });
+const chats = {};
 
-const bot = new TelegramApi(token, {polling: true})
+var client = { phone: "", name: "" , payment: "" };
 
-const chats = {}
+var state = "";
 
-bot.on('message', async msg => {
+bot.on("message", async (msg) => {
   const text = msg.text;
   const chatId = msg.chat.id;
-if (text === '/start') {
-  bot.sendMessage(chatId, "привет", {
-    reply_markup: {
-        "keyboard": [["Ввести продажу"]]
-        }
-    });
-  }
-})
-bot.on('message', async msg => {
   const button = "Ввести продажу";
-  const chatId = msg.chat.id;
-  if (msg.text.indexOf(button) === 0) {
-      bot.sendMessage(chatId, "Введите телефон клиента");
-  }
-  });
 
-  
+  if (text === "/start") {
+    bot.sendMessage(chatId, "привет", {
+      reply_markup: {
+        keyboard: [["Ввести продажу"]],
+      },
+    });
+    state = "";
+  }
+
+  if (text.indexOf(button) === 0) {
+    bot.sendMessage(chatId, "Введите телефон клиента");
+    state = "tel";
+  } else if (state === "tel") {
+    client.phone = text;
+    bot.sendMessage(chatId, "Введите имя клиента");
+    state = "name";
+  } else if (state === 'name') {
+    client.name = text;
+    bot.sendMessage(chatId, "Введите сумму платежа клиента")
+    state = "payment";
+  }
+  else if (state === 'payment') {
+    client.payment = text;
+    bot.sendMessage(chatId, "Имя, телефон и сумма платежа успешно записаны")
+  }
+});
