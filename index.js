d2f4a9029f7bef109d7ab77720d40d76b0840ede
obikym <111091812+obikym@users.noreply.github.com>
2022-11-20 00:02:44 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const TelegramApi = require("node-telegram-bot-api");
 
-const token = "5559401316:AAHOJPbQoonU7b593n3tCYwIo8AKrIcMsGw";
+const token = "5559401316:AAFSI0zmDw34TiwZ2JOMvQ6AZAuIwbKif_k";
 const bot = new TelegramApi(token, { polling: true });
 const chats = {};
 
@@ -20,27 +20,27 @@ bot.on("message", async (msg) => {
       },
     });
     state = "";
-  }
+  };
 
   if (text.indexOf(button) === 0) {
     bot.sendMessage(chatId, "Введите телефон клиента");
     state = "tel";
-  } else if (state === "tel") {
+  } else if (state === "tel") {;
     client.phone = text;
     bot.sendMessage(chatId, "Введите имя клиента");
     state = "name";
   } else if (state === 'name') {
     client.name = text;
-    bot.sendMessage(chatId, "Введите сумму платежа клиента")
+    bot.sendMessage(chatId, "Введите сумму платежа клиента");
     state = "payment";
   }
   else if (state === 'payment') {
     client.payment = text;
-    if (text === Number) {
-    bot.sendMessage(chatId, "Имя, телефон и сумма платежа успешно записаны")
+    if (isNaN(client.payment) === false) {
+    bot.sendMessage(chatId, "Имя, телефон и сумма платежа успешно записаны");          
     }
-    else {
-      bot.sendMessage(chatId, "Проверьте формат ввода!")
+    if (isNaN(client.payment) === true) {
+      bot.sendMessage(chatId, "Проверьте формат ввода!");
     }
   }
 });
