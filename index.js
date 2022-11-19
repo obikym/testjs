3eb16c000c7c546741842c70cca3921ed7f6d492
obikym <111091812+obikym@users.noreply.github.com>
2022-11-19 17:48:10 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const TelegramApi = require("node-telegram-bot-api");
 
-const token = "5559401316:AAEF4BdyWEShX7m-IyMq9CPlnx4BuG1C8z0";
+const token = "5559401316:AAHOJPbQoonU7b593n3tCYwIo8AKrIcMsGw";
 const bot = new TelegramApi(token, { polling: true });
 const chats = {};
 
-var client = { phone: "", name: "" , payment: "" };
+var client = { phone: "", name: "" , payment: ""};
 
 var state = "";
 
@@ -36,6 +36,11 @@ bot.on("message", async (msg) => {
   }
   else if (state === 'payment') {
     client.payment = text;
+    if (text === Number) {
     bot.sendMessage(chatId, "Имя, телефон и сумма платежа успешно записаны")
+    }
+    else {
+      bot.sendMessage(chatId, "Проверьте формат ввода!")
+    }
   }
 });
