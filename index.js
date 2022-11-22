cca8a96dbc58a5b28991f1d5b6736bea57c0099f
obikym <111091812+obikym@users.noreply.github.com>
2022-11-22 19:42:57 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const token = "5559401316:AAFSI0zmDw34TiwZ2JOMvQ6AZAuIwbKif_k";
 const bot = new TelegramApi(token, { polling: true });
 const chats = {};
 
-var client = { phone: "", name: "" , payment: ""};
+var client = { phone: "", name: "" , payment: "" , share: ""};
 
 var state = "";
 
@@ -17,6 +17,7 @@ bot.on("message", async (msg) => {
     bot.sendMessage(chatId, "привет", {
       reply_markup: {
         keyboard: [["Ввести продажу"]],
+        resize_keyboard: true
       },
     });
     state = "";
@@ -37,10 +38,44 @@ bot.on("message", async (msg) => {
   else if (state === 'payment') {
     client.payment = text;
     if (isNaN(client.payment) === false) {
-    bot.sendMessage(chatId, "Имя, телефон и сумма платежа успешно записаны");          
+    bot.sendMessage(chatId, "Имя, телефон и сумма платежа успешно записаны");  
+    state = "share";     
     }
     if (isNaN(client.payment) === true) {
-      bot.sendMessage(chatId, "Проверьте формат ввода!");
+      bot.sendMessage(chatId, "Вы ошиблись, введите число.");
     }
   }
+  if (state === "share") {
+    bot.sendMessage(chatId, "Какую долю вы получаете?", {
+      "reply_markup": {
+          "inline_keyboard": [
+              [
+                  {
+                      text: "30%",
+                      callback_data: "30",
+                  },
+                  {
+                    text: "35%",
+                    callback_data: "35",
+                },
+                {
+                  text: "40%",
+                  callback_data: "40",
+              },
+              ],
+          ],
+          
+      },
+  });
+  state = "";
+   }
+
+});
+
+bot.on("callback_query", (callbackQuery) => {
+  const msg = callbackQuery.message;
+  bot.answerCallbackQuery(callbackQuery.id)
+      .then(() => bot.sendMessage(msg.chat.id, "Успешно записано!"));
+      client.share = callbackQuery.data;
+      console.log(client.share)
 });
