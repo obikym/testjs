447d94dece45fbef8a944cc4c59de20ca31c7f95
obikym <111091812+obikym@users.noreply.github.com>
2022-11-27 17:39:03 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const token = "5559401316:AAFSI0zmDw34TiwZ2JOMvQ6AZAuIwbKif_k";
 const bot = new TelegramApi(token, { polling: true });
 const chats = {};
 
-var client = { phone: "", name: "" , payment: "" , share: ""};
+var client = { phone: "", name: "", payment: "", share: "", pure: "" };
 
 var state = "";
 
@@ -26,7 +26,8 @@ bot.on("message", async (msg) => {
   if (text.indexOf(button) === 0) {
     bot.sendMessage(chatId, "Введите телефон клиента");
     state = "tel";
-  } else if (state === "tel") {;
+  } else if (state === "tel") {
+    ;
     client.phone = text;
     bot.sendMessage(chatId, "Введите имя клиента");
     state = "name";
@@ -38,8 +39,8 @@ bot.on("message", async (msg) => {
   else if (state === 'payment') {
     client.payment = text;
     if (isNaN(client.payment) === false) {
-    bot.sendMessage(chatId, "Имя, телефон и сумма платежа успешно записаны");  
-    state = "share";     
+      bot.sendMessage(chatId, "Имя, телефон и сумма платежа успешно записаны");
+      state = "share";
     }
     if (isNaN(client.payment) === true) {
       bot.sendMessage(chatId, "Вы ошиблись, введите число.");
@@ -48,34 +49,31 @@ bot.on("message", async (msg) => {
   if (state === "share") {
     bot.sendMessage(chatId, "Какую долю вы получаете?", {
       "reply_markup": {
-          "inline_keyboard": [
-              [
-                  {
-                      text: "30%",
-                      callback_data: "30",
-                  },
-                  {
-                    text: "35%",
-                    callback_data: "35",
-                },
-                {
-                  text: "40%",
-                  callback_data: "40",
-              },
-              ],
+        "inline_keyboard": [
+          [
+            {
+              text: "30%",
+              callback_data: "30",
+            },
+            {
+              text: "35%",
+              callback_data: "35",
+            },
+            {
+              text: "40%",
+              callback_data: "40",
+            },
           ],
-          
+        ],
+
       },
-  });
-  state = "";
-   }
-
+    });
+    state = "pure";
+  }
 });
-
 bot.on("callback_query", (callbackQuery) => {
-  const msg = callbackQuery.message;
   bot.answerCallbackQuery(callbackQuery.id)
-      .then(() => bot.sendMessage(msg.chat.id, "Успешно записано!"));
-      client.share = callbackQuery.data;
-      console.log(client.share)
-});
+  client.share = callbackQuery.data;
+  client.pure = client.payment / 100 * Number(client.share)
+  bot.sendMessage(callbackQuery.message.chat.id, "Вы заработали - " + client.pure + "р");
+})
